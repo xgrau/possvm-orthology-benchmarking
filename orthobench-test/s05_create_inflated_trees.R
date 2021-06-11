2973b6dcfe6eb960ef3cbc79463ae3fe21512b7c
xgrau <xavier.graubove@gmail.com>
2021-06-11 19:47:44 +0200

    inflate a fraction of reads at random length, variable factor

--- orthobench-test/s05_create_inflated_trees.R
+++ orthobench-test/s05_create_inflated_trees.R
@@ -13,7 +13,8 @@ ort_sets = list(
 )
 
 num_iter = 20
-inflation_factor = 10
+edge_fraction = 0.05
+inflation_factor_bounds = c(5,20)
 
 # load data
 ref = read.table(ref_fn, sep="\t", header = F, col.names = c("refOG", "gene"), stringsAsFactors=F)
@@ -34,10 +35,17 @@ for (ort_set in ort_sets) {
     
     for (it in 1:num_iter) {
       
-      # inflate one edge
+      # get tree
       tri = tre
-      ix = sample(1:length(tri$edge.length), size = 1)
-      tri$edge.length [ ix ] = tri$edge.length [ ix ] * inflation_factor
+      
+      # alter length of a fixed % of edges
+      num_edges_to_alter = floor(length(tri$edge.length) * edge_fraction)
+      ixs = sample(1:length(tri$edge.length), size = num_edges_to_alter)
+      
+      for (ix in ixs) {
+        inflation_factor = runif(1, min = inflation_factor_bounds[1], max = inflation_factor_bounds[2])
+        tri$edge.length [ ix ] = tri$edge.length [ ix ] * inflation_factor
+      }
       
       # save tree
       ape::write.tree(tri, file = sprintf("%s/%s_it%i.tre", out_fn, fam, it))
