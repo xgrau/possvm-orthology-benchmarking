4fb747a26ff785fafc49cc371c870d229f1f9d4d
xgrau <xavier.graubove@gmail.com>
2021-06-16 15:16:02 +0200

    master evaluation script for orthobench

--- orthobench-test/s06_root_test_evaluate_iterroot.R
+++ orthobench-test/s06_root_test_evaluate_iterroot.R
@@ -17,11 +17,8 @@ ref$species = stringr::str_split(ref$gene, pattern="_", simplify = T)[,1]
 fam_list = unique(ref$refOG)
 sps_list = unique(ref$species)
 
-num_iter = 20
-# fam_list = fam_list[1:60]
 
-
-#### all trees ####
+#### Loop: accuracy per tree ####
 
 # loop
 
@@ -39,6 +36,7 @@ for (ort_fn in list_ort_files) {
   # get root hash
   phy_fn = sprintf("%s/%s", ort_fo, ort_phy_fn)
   phy = ape::read.tree(phy_fn)
+  phy = ape::ladderize(phy)
   phy$node.label = NULL
   root_descendants_ix = phangorn::Descendants(phy, phy$edge[1,2])[[1]]
   root_descendants = phy$tip.label [ root_descendants_ix ]
@@ -46,8 +44,8 @@ for (ort_fn in list_ort_files) {
   root_descendants_hash = sapply(root_descendants, digest, algo="md5")
   
   # read in possvm classification
-  ort = read.table(sprintf("%s/%s", ort_fo, ort_fn), sep="\t", header = T, stringsAsFactors = F)
-  ort$species = stringr::str_split(ort$gene, pattern = "_", simplify = T)[,1]
+  ort = read.table(sprintf("%s/%s", ort_fo, ort_fn), sep="\t", header = TRUE, stringsAsFactors = FALSE)
+  ort$species = stringr::str_split(ort$gene, pattern = "_", simplify = TRUE)[,1]
   ort = ort[ort$species %in% sps_list,]
   
   # subset ref to refOG of interest
@@ -121,54 +119,135 @@ for (ort_fn in list_ort_files) {
 
 
 
-
-#### Merged analysis ####
+#### Parallel analysis: mid & ite ####
 
 dia_mid = dia [ dia$method == "mid", ]
 dia_ite = dia [ dia$method == "ite", ]
 
 # merge
 dit = merge(dia_mid, dia_ite, by.x = "iteration_id", by.y = "iteration_id", all.x = FALSE, all.y = FALSE, suffixes = c(".mid", ".ite"))
+# dit = dit[ dit$inflated_fraction.ite != "f0.100" , ]
 
-# calculate difs?
+# calculate change in recall, precision and Fscore
 dit$recall_diff = dit$recall.ite - dit$recall.mid
 dit$precision_diff = dit$precision.ite - dit$precision.mid
 dit$Fscore_diff = dit$Fscore.ite - dit$Fscore.mid
 
-
 # variable tree: there's been a change in fscore?
-ixs_variable = which(dit$Fscore_diff != 0)
-
+ixs_fscd = abs(dit$Fscore_diff) > 0
 # variable tree: there's been a change in root hash?
-ixs_variable = which(as.character(dit$root.ite) != as.character(dit$root.mid))
+ixs_variable = as.character(dit$root.ite) != as.character(dit$root.mid)
+# ixs_variable = which(as.character(dit$root.ite) != as.character(dit$root.mid) & abs(dit$Fscore_diff) > 0)
+
+
+
+# add factor with inflation method
+dit$inflation_method = apply(stringr::str_split(dit$iteration_id, " ", simplify = TRUE)[,3:4], 1, function(x) paste(x, collapse = " "))
+dit$inflation_method = factor(dit$inflation_method, levels = sort(unique(dit$inflation_method)))
+
+
+
+pdf("results_evaluation/difference_iter_mid_inflation.pdf", height = 5, width = 5)
+par(mar = c(10.1, 4.1, 4.1, 2.1))
+
+# table: number of recall or precision improvements caused by iterative rooting
+dit_sum = data.frame(
+  row.names  =    levels(dit$inflation_method),
+  d_recall_p =    aggregate(recall_diff ~ inflation_method, data = dit[ixs_variable,], FUN = function(x) sum(x>0))[,2],
+  d_recall_n =    aggregate(recall_diff ~ inflation_method, data = dit[ixs_variable,], FUN = function(x) sum(x<0))[,2] * -1,
+  d_precision_p = aggregate(precision_diff ~ inflation_method, data = dit[ixs_variable,], FUN = function(x) sum(x>0))[,2],
+  d_precision_n = aggregate(precision_diff ~ inflation_method, data = dit[ixs_variable,], FUN = function(x) sum(x<0))[,2] * -1
+)
+# barplots
+barplot(
+  t(as.matrix(dit_sum))[c(1,3),], 
+  beside = TRUE, las = 2, 
+  ylim = c(-100, 160),
+  col = c("blue","skyblue"), 
+  ylab = "# changes in precision or recall")
+barplot(
+  t(as.matrix(dit_sum))[c(2,4),], 
+  beside = TRUE, axes = FALSE, axisnames = FALSE,
+  col = c("deeppink3","pink1"), 
+  add = TRUE)
+legend(1,170, cex = 0.7, legend = c("ite improves recall", "ite improves precision", "ite worsens recall", "ite worsens precision"), fill=c("blue","skyblue","deeppink3","pink1"), bty="n")
+title(main = "Change in recall and precision in inflated trees")
+
+
+# table: median of recall or precision improvement caused by iterative rooting
+dit_mag = data.frame(
+  row.names  =    levels(dit$inflation_method),
+  d_recall_p =    aggregate(recall_diff ~ inflation_method, data = dit[ixs_variable,], FUN = function(x) median(x[x>0]) )[,2],
+  d_recall_n =    aggregate(recall_diff ~ inflation_method, data = dit[ixs_variable,], FUN = function(x) median(x[x<0]) )[,2],
+  d_precision_p = aggregate(precision_diff ~ inflation_method, data = dit[ixs_variable,], FUN = function(x) median(x[x>0]) )[,2],
+  d_precision_n = aggregate(precision_diff ~ inflation_method, data = dit[ixs_variable,], FUN = function(x) median(x[x<0]) )[,2]
+)
+
+
+# barplots
+barplot(
+  t(as.matrix(dit_mag))[c(1,3),], 
+  beside = TRUE, las = 2, 
+  ylim = c(-1, 1),
+  col = c("blue","skyblue"), 
+  ylab = "sum(ite-mid)")
+barplot(
+  t(as.matrix(dit_mag))[c(2,4),], 
+  beside = TRUE, axes = FALSE, axisnames = FALSE,
+  col = c("deeppink3","pink1"), 
+  add = TRUE)
+legend("topleft", cex = 0.7, legend = c("ite improves recall", "ite improves precision", "ite worsens recall", "ite worsens precision"), fill=c("blue","skyblue","deeppink3","pink1"), bty="n")
+title(main = "Change in recall and precision in inflated trees")
+
+
+
+# final values
+# distributions
+vioplot::vioplot(Fscore.mid ~ inflation_method, data = dit[ixs_variable,], side="left",  col = "pink1",  colMed = "deeppink3", rectCol = "deeppink4", lineCol = "deeppink4", las = 2 , ylim = c(0,1), ylab = "F-score")
+vioplot::vioplot(Fscore.ite ~ inflation_method, data = dit[ixs_variable,], side="right", col = "skyblue",colMed = "blue",      rectCol = "blue4",     lineCol = "blue4",     add= TRUE)
+legend("bottomleft", cex = 0.7, legend = c("ite", "mid"), fill=c("skyblue","pink1"), bty="n")
+title("F-score per inflation method")
+
+vioplot::vioplot(precision.mid ~ inflation_method, data = dit[ixs_variable,], side="left",  col = "pink1",  colMed = "deeppink3", rectCol = "deeppink4", lineCol = "deeppink4", las = 2 , ylim = c(0,1), ylab = "Precision")
+vioplot::vioplot(precision.ite ~ inflation_method, data = dit[ixs_variable,], side="right", col = "skyblue",colMed = "blue",      rectCol = "blue4",     lineCol = "blue4",     add= TRUE)
+legend("bottomleft", cex = 0.7, legend = c("ite", "mid"), fill=c("skyblue","pink1"), bty="n")
+title("Precision per inflation method")
+
+vioplot::vioplot(recall.mid ~ inflation_method, data = dit[ixs_variable,], side="left",  col = "pink1",  colMed = "deeppink3", rectCol = "deeppink4", lineCol = "deeppink4", las = 2 , ylim = c(0,1), ylab = "Recall")
+vioplot::vioplot(recall.ite ~ inflation_method, data = dit[ixs_variable,], side="right", col = "skyblue",colMed = "blue",      rectCol = "blue4",     lineCol = "blue4",     add= TRUE)
+legend("bottomleft", cex = 0.7, legend = c("ite", "mid"), fill=c("skyblue","pink1"), bty="n")
+title("Recall per inflation method")
+
 
 
 # plots
-pdf("results_evaluation/difference_iter_mid_inflation.pdf", height = 4, width = 4)
-
 breaks = seq(from = -1, to = 1, length.out = 40)
-
 # change in fscore
-hist(dit$Fscore_diff[ixs_variable], breaks = breaks, col="gray90", xlim = c(-1,1), main = "Fscore in variable trees", xlab = "d(ite-mid)", ylim=c(0,100))
+hist(dit$Fscore_diff[ixs_variable], breaks = breaks, col="gray90", xlim = c(-1,1), main = "Fscore in variable trees", xlab = "d(ite-mid)", ylim=c(0,200))
 title(sub=sprintf(
-  "f>0 = %i | f<0 = %i", sum(dit$Fscore_diff > 0 ), sum(dit$Fscore_diff < 0 )
+  "ite improves = %i | ite worsens = %i | n = %i", sum(dit$Fscore_diff[ixs_variable] > 0), sum(dit$Fscore_diff[ixs_variable] < 0), length(ixs_variable)
 ))
 abline(v=0, lty=2, col="red")
 
 # change in precision
-hist(dit$precision_diff[ixs_variable], breaks = breaks, col="gray90", xlim = c(-1,1), main = "Precision in variable trees", xlab = "d(ite-mid)",ylim=c(0,100))
+hist(dit$precision_diff[ixs_variable], breaks = breaks, col="gray90", xlim = c(-1,1), main = "Precision in variable trees", xlab = "d(ite-mid)",ylim=c(0,200))
 title(sub=sprintf(
-  "p>0 = %i | p<0 = %i", sum(dit$precision_diff > 0 ), sum(dit$precision_diff < 0 )
+  "ite improves = %i | ite worsens = %i | n = %i", sum(dit$precision_diff[ixs_variable] > 0), sum(dit$precision_diff[ixs_variable] < 0), length(ixs_variable)
 ))
 abline(v=0, lty=2, col="red")
 
 # change in recall
-hist(dit$recall_diff[ixs_variable], breaks = breaks, col="gray90", xlim = c(-1,1), main = "Recall in variable trees", xlab = "d(ite-mid)", ylim=c(0,100))
+hist(dit$recall_diff[ixs_variable], breaks = breaks, col="gray90", xlim = c(-1,1), main = "Recall in variable trees", xlab = "d(ite-mid)", ylim=c(0,200))
 title(sub=sprintf(
-  "r>0 = %i | r<0 = %i", sum(dit$recall_diff > 0 ), sum(dit$recall_diff < 0 )
+  "ite improves = %i | ite worsens = %i | n = %i", sum(dit$recall_diff[ixs_variable] > 0), sum(dit$recall_diff[ixs_variable] < 0), length(ixs_variable)
 ))
 abline(v=0, lty=2, col="red")
 
 dev.off()
 
+
+
+# save?
 write.table(dit, "results_evaluation/difference_iter_mid_inflation.csv", quote = FALSE, sep = "\t", row.names = FALSE)
+
+
